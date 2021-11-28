1618debcca23048add4c6a881984c0fe079152f7
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2021-11-28 18:28:25 +0545

    add sample mock data for the sun

---
+++ final/solarium/lib/models/event.dart
@@ -0,0 +1,10 @@
+class Event {
+  String name;
+  String launchDate;
+  String description;
+
+  Event(
+      {required this.name,
+      required this.launchDate,
+      required this.description});
+}
