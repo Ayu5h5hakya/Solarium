2693c39f25d27e9fd309f56bd5cfc1dc33e00b6d
Ayush Shakya <ashakya@hamropatro.com>
2021-11-28 12:39:14 +0545

    add    sun widget

--- final/solarium/lib/sun.dart
+++ final/solarium/lib/sun.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Sun extends StatelessWidget {
-  const Sun({Key? key}) : super(key: key);
+  final double? size;
+  const Sun({Key? key, this.size}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: size ?? 90,
+      height: size ?? 90,
+      decoration:
+          BoxDecoration(shape: BoxShape.circle, color: Colors.yellow[100]),
+    );
   }
 }
