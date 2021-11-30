4b227f4abbe4ca992a1ae7e74e1623b510101387
AyushShakya <ashakya@hamropatro.com>
2021-11-30 17:34:02 +0545

    add sun sample data in assets folders

--- final/solarium/lib/models/solar_member.dart
+++ final/solarium/lib/models/solar_member.dart
@@ -1,7 +1,6 @@
 import 'package:solarium/models/event.dart';
 import 'package:solarium/models/media_presence.dart';
 import 'package:solarium/models/moon.dart';
-import 'package:solarium/models/random_fact.dart';
 
 class SolarMember {
   String name;
@@ -10,14 +9,23 @@ class SolarMember {
   List<Moon> moons;
   List<MediaPresence> mediaPresence;
   List<Event> events;
-  List<RandomFact> facts;
 
-  SolarMember(
-      {required this.name,
-      required this.tempInK,
-      required this.radInKm,
-      required this.moons,
-      required this.mediaPresence,
-      required this.events,
-      required this.facts});
+  SolarMember({
+    required this.name,
+    required this.tempInK,
+    required this.radInKm,
+    required this.moons,
+    required this.mediaPresence,
+    required this.events,
+  });
+
+  static SolarMember fromJson(Map<String, dynamic> json) {
+    return SolarMember(
+        name: json['name'] as String? ?? "",
+        tempInK: json['tempInK'] as int? ?? 0,
+        radInKm: json['radInKm'] as int? ?? 0,
+        moons: Moon.fromJson(json['moons']),
+        mediaPresence: MediaPresence.fromJson(json['popculture']),
+        events: Event.fromJson(json['event']));
+  }
 }
