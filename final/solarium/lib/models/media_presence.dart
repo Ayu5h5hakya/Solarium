1b6341fd0659fdf0909da6cc2b948e3636c0f75b
Ayush Shakya <ashakya@hamropatro.com>
2021-12-01 16:14:28 +0545

    finalize movie locations page view design

--- final/solarium/lib/models/media_presence.dart
+++ final/solarium/lib/models/media_presence.dart
@@ -1,14 +1,25 @@
 class MediaPresence {
   String name;
   String genre;
+  String cast;
+  String director;
   String synopsis;
+  String imageUrl;
+  String backdropUrl;
+  String rating;
   int year;
 
-  MediaPresence(
-      {required this.name,
-      required this.genre,
-      required this.synopsis,
-      required this.year});
+  MediaPresence({
+    required this.name,
+    required this.genre,
+    required this.cast,
+    required this.director,
+    required this.synopsis,
+    required this.year,
+    required this.rating,
+    required this.imageUrl,
+    required this.backdropUrl,
+  });
 
   static List<MediaPresence> fromJson(List<dynamic>? json) {
     List<MediaPresence> _result = [];
@@ -17,7 +28,12 @@ class MediaPresence {
         return MediaPresence(
           name: value['name'] as String? ?? "",
           genre: value['genre'] as String? ?? "",
+          cast: value['cast'] as String? ?? "",
+          director: value['director'] as String? ?? "",
           synopsis: value['synopsis'] as String? ?? "",
+          imageUrl: value['imageUrl'] as String? ?? "",
+          rating: value['rating'] as String? ?? "",
+          backdropUrl: value['backdropUrl'] as String? ?? "",
           year: value['year'] as int? ?? 0,
         );
       }).toList();
--- final/solarium/lib/widgets/movie_location_pageview.dart
+++ final/solarium/lib/widgets/movie_location_pageview.dart
@@ -1,61 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:solarium/models/media_presence.dart';
 
 class MovieLocationPageView extends StatelessWidget {
-  const MovieLocationPageView({Key? key}) : super(key: key);
+  final List<MediaPresence> data;
+  const MovieLocationPageView({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-        Column(
-          children: [
-            FlutterLogo(),
-            Expanded(
-              child: ListView(
+    return PageView.builder(
+      itemBuilder: (_, index) {
+        final _item = data[index];
+        return Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
+                  Image.network(
+                    _item.imageUrl,
+                    height: 120.0,
                   ),
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
+                  const SizedBox(
+                    width: 10.0,
                   ),
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(fontSize: 40),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          '${_item.name}',
+                          maxLines: 2,
+                          style: TextStyle(fontSize: 40),
+                        ),
+                        Text(
+                          '${_item.year}',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            )
-          ],
-        )
-      ],
+              Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      dense: true,
+                      title: Text('Cast'),
+                      subtitle: Text(_item.cast),
+                    ),
+                    ListTile(
+                      dense: true,
+                      title: Text('Genre'),
+                      subtitle: Text(_item.genre),
+                    ),
+                    ListTile(
+                      dense: true,
+                      title: Text('Director'),
+                      subtitle: Text(_item.director),
+                    ),
+                    ListTile(
+                      title: Text(_item.synopsis),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+      itemCount: data.length,
     );
   }
 }
