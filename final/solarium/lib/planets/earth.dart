956f165580ce3d993829277721b5f196108da5f0
Ayush Shakya <ashakya@hamropatro.com>
2021-12-01 13:13:07 +0545

    add asset images for the planets and the sun

--- final/solarium/lib/planets/earth.dart
+++ final/solarium/lib/planets/earth.dart
@@ -10,8 +10,11 @@ class Earth extends StatelessWidget {
     return Container(
       width: size ?? 90,
       height: size ?? 90,
-      decoration:
-          BoxDecoration(shape: BoxShape.circle, color: Colors.yellow[100]),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/earth.png'),
+        ),
+      ),
     );
   }
 }
