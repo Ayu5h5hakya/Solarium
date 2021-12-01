ebbb814d60ce7bab12ec774c690745635c218c30
Ayush Shakya <ashakya@hamropatro.com>
2021-12-01 19:55:23 +0545

    ensure project iscompliant with razeware guidelines

--- final/solarium/lib/details.dart
+++ final/solarium/lib/details.dart
@@ -1,10 +1,37 @@
-import 'package:solarium/constants.dart';
-import 'package:solarium/widgets/body_surface_data.dart';
-import 'package:solarium/widgets/moons_grid.dart';
-import 'package:solarium/widgets/movie_location_pageview.dart';
-import 'package:solarium/repository/solar_repository.dart';
-import 'package:solarium/widgets/solar_events_pageview.dart';
-import 'package:solarium/sun.dart';
+// Copyright (c) 2021 Razeware LLC
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+// distribute, sublicense, create a derivative work, and/or sell copies of the
+// Software in any work that is designed, intended, or marketed for pedagogical
+// or instructional purposes related to programming, coding, application
+// development or information technology.  Permission for such use, copying,
+// modification, merger, publication, distribution, sublicensing, creation of
+// derivative works, or sale is expressly withheld.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import 'constants.dart';
+import 'widgets/body_surface_data.dart';
+import 'widgets/moons_grid.dart';
+import 'widgets/movie_location_pageview.dart';
+import 'repository/solar_repository.dart';
+import 'widgets/solar_events_pageview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
--- final/solarium/lib/models/solar_member.dart
+++ final/solarium/lib/models/solar_member.dart
@@ -1,6 +1,34 @@
-import 'package:solarium/models/event.dart';
-import 'package:solarium/models/media_presence.dart';
-import 'package:solarium/models/moon.dart';
+// Copyright (c) 2021 Razeware LLC
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+// distribute, sublicense, create a derivative work, and/or sell copies of the
+// Software in any work that is designed, intended, or marketed for pedagogical
+// or instructional purposes related to programming, coding, application
+// development or information technology.  Permission for such use, copying,
+// modification, merger, publication, distribution, sublicensing, creation of
+// derivative works, or sale is expressly withheld.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import 'event.dart';
+import 'media_presence.dart';
+import 'moon.dart';
 
 class SolarMember {
   String name;
@@ -21,11 +49,12 @@ class SolarMember {
 
   static SolarMember fromJson(Map<String, dynamic> json) {
     return SolarMember(
-        name: json['name'] as String? ?? "",
+        name: json['name'] as String? ?? '',
         tempInK: json['tempInK'] as int? ?? 0,
         radInKm: json['radInKm'] as int? ?? 0,
-        moons: Moon.fromJson(json['moons']),
-        mediaPresence: MediaPresence.fromJson(json['popculture']),
-        events: Event.fromJson(json['event']));
+        moons: Moon.fromJson(json['moons'] as List<dynamic>),
+        mediaPresence:
+            MediaPresence.fromJson(json['popculture'] as List<dynamic>),
+        events: Event.fromJson(json['event'] as List<dynamic>));
   }
 }
